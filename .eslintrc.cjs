220a011e8489f05bf76b3d017defba2ca2dbfb48
tuchongyang <779311998@qq.com>
2023-08-29 17:50:07 +0800

    updat eeslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
     sourceType: "module",
   },
   extends: ["plugin:vue/vue3-recommended", "plugin:@typescript-eslint/recommended", "prettier", "plugin:prettier/recommended"], //相当于，用 prettier 的规则，覆盖掉 eslint:recommended 的部分规则。Version 8.0.0 (2021-02-21)Changed: All configs have been merged into one!
-  plugins: ["prettier"], //eslint-plugin-prettier 将 prettier 的能力集成到 eslint 中。按照 prettier 的规则检查代码规范性，并进行修复。
+  // plugins: ["prettier"], //eslint-plugin-prettier 将 prettier 的能力集成到 eslint 中。按照 prettier 的规则检查代码规范性，并进行修复。
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
