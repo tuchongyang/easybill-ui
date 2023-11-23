a01d5478122386132821ca0d6281b541d3cc9eb3
tuchongyang <779311998@qq.com>
2023-11-23 10:32:51 +0800

    完善文档

--- docs/.vitepress/src/sidebar/index.ts
+++ docs/.vitepress/src/sidebar/index.ts
@@ -19,12 +19,70 @@ export default {
     {
       text: "DetailInfo",
       items: [
-        { text: "基础用法", link: "/components/detail-info" },
+        { text: "基础用法", link: "/components/detail-info/" },
         { text: "不同的风格", link: "/components/detail-info/type" },
         { text: "自定义组件", link: "/components/detail-info/component" },
         { text: "HTML渲染", link: "/components/detail-info/html" },
         { text: "插槽", link: "/components/detail-info/slot" },
-        { text: "属性", link: "/components/detail-infoprops" },
+        { text: "属性", link: "/components/detail-info/props" },
+      ],
+    },
+  ],
+  "/components/curd-form/": [
+    {
+      text: "CurdForm",
+      items: [
+        { text: "基础用法", link: "/components/curd-form/" },
+        { text: "对齐方式", link: "/components/curd-form/align" },
+        { text: "栅格", link: "/components/curd-form/span" },
+        { text: "表单校验", link: "/components/curd-form/valid" },
+        { text: "插槽", link: "/components/curd-form/slot" },
+        {
+          text: "内置组件",
+          items: [
+            { text: "input输入框", link: "/components/curd-form/input" },
+            { text: "Select选择器", link: "/components/curd-form/select" },
+          ],
+        },
+        { text: "自定义组件", link: "/components/curd-form/component" },
+        { text: "使用全局组件", link: "/components/curd-form/global" },
+
+        { text: "属性", link: "/components/curd-formprops" },
+      ],
+    },
+  ],
+  "/components/curd-table/": [
+    {
+      text: "CurdTable",
+      items: [
+        { text: "基础用法", link: "/components/curd-table/" },
+        { text: "多级表头", link: "/components/curd-table/multi-header" },
+        { text: "固定列", link: "/components/curd-table/fixed" },
+        { text: "多选", link: "/components/curd-table/checkbox" },
+        { text: "插槽", link: "/components/curd-table/slot" },
+        { text: "展开行", link: "/components/curd-table/expand" },
+        { text: "属性", link: "/components/curd-table/props" },
+      ],
+    },
+  ],
+  "/components/table-filter/": [
+    {
+      text: "TableFilter",
+      items: [
+        { text: "基础用法", link: "/components/table-filter/" },
+        { text: "外部项", link: "/components/table-filter/type" },
+        { text: "内置组件", link: "/components/table-filter/component" },
+        { text: "插槽", link: "/components/table-filter/slot" },
+        { text: "属性", link: "/components/table-filter/props" },
+      ],
+    },
+  ],
+  "/components/form-dialog/": [
+    {
+      text: "FormDialog",
+      items: [
+        { text: "基础用法", link: "/components/form-dialog/" },
+        { text: "属性", link: "/components/form-dialog/props" },
       ],
     },
   ],
