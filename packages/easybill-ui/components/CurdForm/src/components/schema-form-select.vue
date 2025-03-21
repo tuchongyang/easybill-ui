f0a2b7c88a4bf4d5720e5dd17be8b3f07709a45b
tuchongyang <779311998@qq.com>
2025-03-21 14:08:08 +0800

    fix: 修复select组件props问题

--- packages/easybill-ui/components/CurdForm/src/components/schema-form-select.vue
+++ packages/easybill-ui/components/CurdForm/src/components/schema-form-select.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-select v-model="model" style="width: 100%" :loading="formItem.loading || loading" :placeholder="'请选择' + formItem.label" v-bind="selectProps" v-on="eventObject">
+  <el-select v-model="model" style="width: 100%" :loading="formItem.loading || loading" :placeholder="'请选择' + formItem.label" v-bind="props" v-on="eventObject">
     <el-option v-if="props.all && !props.multiple" value="" label="全部"></el-option>
     <el-checkbox v-if="props.all && props.multiple" v-model="checked" label="全选" class="schema-form-select-check" />
     <template v-for="option in list" :key="option.value">
